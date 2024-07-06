dfc9453ef4c7cd4c4cd1773e3726f8cf581fbc1e
gkumar-pandey <gautamshekhar078@gmail.com>
2024-07-06 17:00:25 +0530

    feat:added redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Countrydetails from "./Pages/Countrydetails";
-import Home from "./Components/ Home";
 import { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import HomePage from "./Pages/HomePage";
 import { CssBaseline } from "@mui/material";
+
 function App() {
   const [isDarkMode, setDarkMode] = useState(false);
   const HandleDarkMode = () => {
@@ -33,12 +34,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-
       <Router>
         <Navbar isDarkMode={isDarkMode} HandleDarkMode={HandleDarkMode} />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail/:id" element={<Countrydetails />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:name" element={<Countrydetails />} />
         </Routes>
       </Router>
     </ThemeProvider>
